e7a6132562af7ed3072627eea621147010b41895
Phearun Ol <phearun.ph77@gmail.com>
2023-12-28 11:18:39 +0700

    :passport_control: Book update by id

--- src/main/java/itc/i4gic/demo_jpa/controller/BookController.java
+++ src/main/java/itc/i4gic/demo_jpa/controller/BookController.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import itc.i4gic.demo_jpa.entity.Book;
 import itc.i4gic.demo_jpa.service.BookService;
+
 
 
 
@@ -42,4 +46,12 @@ public class BookController {
       service.save(b);
       return "redirect:/available_books";
    }
+
+   @RequestMapping("/editBook/{id}")
+   public String editBook(@PathVariable("id") int id, Model model) {
+      Book b= service.getBookById(id);
+      model.addAttribute("book", b);
+      return "bookEdit";
+   }
+   
 }
